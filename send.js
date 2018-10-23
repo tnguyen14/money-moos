541e90c257f205750a7f4154001687051cb7f7af
Tri Nguyen <tri@tridnguyen.com>
2018-10-23 01:09:14 -0400

    simple messenger webhook
    - token verification
    - echo

---
+++ send.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const debug = require('debug')('mmoos:send');
+
+module.exports = {
+	text: text
+};
+
+function text (sender, text, cb) {
+	request({
+		url: 'https://graph.facebook.com/v2.7/me/messages',
+		method: 'POST',
+		qs: {
+			access_token: process.env.PAGE_TOKEN
+		},
+		json: {
+			recipient: {
+				id: sender
+			},
+			message: {
+				text: text
+			}
+		}
+	}, function (error, response, body) {
+		if (error) {
+			debug('Error sending message: ', error);
+			cb(error);
+		} else if (response.body.error) {
+			debug('Error: ', response.body.error);
+			cb(response.body.error);
+		} else {
+			cb(null, body);
+		}
+	});
+}
