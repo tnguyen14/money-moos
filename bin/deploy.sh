6f4b0c6a9e89dba7c0a3be707ec443d3741a7749
Tri Nguyen <tri@tridnguyen.com>
2018-10-23 02:27:55 -0400

    turn on debugging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 gcloud beta functions deploy messenger --stage-bucket money-moos-functions \
-    --update-env-vars VERIFY_TOKEN=$VERIFY_TOKEN,PAGE_TOKEN=$PAGE_TOKEN \
+    --update-env-vars VERIFY_TOKEN=$VERIFY_TOKEN,PAGE_TOKEN=$PAGE_TOKEN,DEBUG=mmoos:* \
     --trigger-http
