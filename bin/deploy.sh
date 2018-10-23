48877a39675db9bd6bada95f7d3657206489f92f
Tri Nguyen <tri@tridnguyen.com>
2018-10-23 02:02:39 -0400

    setting env vars is only available in beta

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
-gcloud functions deploy messenger --stage-bucket money-moos-functions \
-    --set-env-vars VERIFY_TOKEN=$VERIFY_TOKEN,PAGE_TOKEN=$PAGE_TOKEN \
+gcloud beta functions deploy messenger --stage-bucket money-moos-functions \
+    --update-env-vars VERIFY_TOKEN=$VERIFY_TOKEN,PAGE_TOKEN=$PAGE_TOKEN \
     --trigger-http
